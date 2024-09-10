c45a43423b47b498f0411fd4dd367e0d541c3336
Matias Larumbe <matias_larumbe@hotmail.com>
2024-09-10 10:01:07 -0300

    Creacion de iconos de especialidades

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { assets, specialityData } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div>SpecialityMenu</div>
-  )
-}
+    <div
+      className="flex flex-col items-center gap-4 py-16 text-gray-800 "
+      id="speciality"
+    >
+      <h1 className="text-xl font-medium">Buscar por especialidad</h1>
+      <p className="sm:w:1/3 text-center text-sm">
+        Simplemente navegue por nuestra extensa lista de médicos de confianza,
+        programe su turno sin problemas
+      </p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item, index) => (
+          <Link onClick={()=>scrollTo(0,0)} className="flex flex-col item-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500 " key={index} to={`/doctors/${item.speciality}`}>
+            <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
+            <p className="text-center">{item.speciality} </p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default SpecialityMenu
+export default SpecialityMenu;
