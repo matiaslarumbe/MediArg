54ff274a7448475dfec23776727a8568e88fc681
Matias Larumbe <matias_larumbe@hotmail.com>
2024-09-28 15:53:56 -0300

    Creacion de About y modificacion de ReltedDoctors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ACERCA DE NOSOTROS{" "}
+          <span className="text-gray-700 font-medium">AR</span>
+        </p>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img
+          className="w-full md:max-w-[360px] "
+          src={assets.about_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+          <b className="text-gray-800">Bienvenidos a MediArg</b>
+          <p>
+            En MediArg, nuestra misión es conectar a los pacientes con los
+            mejores médicos en una variedad de especialidades. A través de una
+            plataforma moderna y fácil de usar, ofrecemos un servicio ágil y
+            accesible para que puedas agendar turnos médicos de manera rápida y
+            sencilla.
+          </p>
+          <p>
+            Nuestra plataforma utiliza tecnología avanzada para facilitar la
+            interacción entre pacientes y profesionales de la salud. Puedes
+            buscar médicos por especialidad, ver sus franjas horarias
+            disponibles, y reservar un turno en cuestión de minutos. Además,
+            MediArg está optimizado para ofrecerte la mejor experiencia, tanto
+            en dispositivos móviles como en tu computadora.
+          </p>
+          <b className="text-gray-800">Nuestra Vision</b>
+          <p>
+            En MediArg, creemos que la tecnología es una herramienta clave para
+            mejorar la atención médica. Nuestra visión es crear una red de
+            atención médica más eficiente, donde los pacientes puedan acceder a
+            servicios médicos de calidad sin barreras de tiempo o ubicación. Nos
+            esforzamos por ofrecer una atención personalizada y accesible,
+            apoyándonos en la innovación tecnológica para simplificar el proceso
+            de agendar y gestionar consultas médicas.
+          </p>
+        </div>
+      </div>
 
-export default About
+      <div className="text-xl my-4">
+        <p>
+          PORQUE{" "}
+          <span className="text-gray-700 font-semibold">ELEGIR MEDIARG</span>{" "}
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer ">
+          <b>Eficiencia</b>
+          <p>
+            Agendá tu turno de manera rápida y sencilla, con solo unos clics y
+            sin perder tiempo en largas esperas.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer ">
+          <b>Conveniencia</b>
+          <p>
+            Accedé a una amplia red de médicos desde cualquier lugar, a
+            cualquier hora, y con la flexibilidad de elegir las franjas horarias
+            que mejor se adapten a vos.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer ">
+          <b>Personalización</b>
+          <p>
+            Encontrá especialistas que se ajusten a tus necesidades específicas
+            y recibí atención médica personalizada en todo momento.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -2,57 +2,289 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/Relateddoctors'
 
 const Appointment = () => {
+  const { docId } = useParams() // Obtener el ID del doctor desde la URL
+  const { doctors, currencySymbol } = useContext(AppContext)
+  const daysOfWeek = ['LUN', 'MAR', 'MIE', 'JUE', 'VIE']
 
-  const {docId} = useParams()
-  const {doctors} = useContext(AppContext)
+  const [docInfo, setDocInfo] = useState(null)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
-  const [docInfo,setDocInfo] = useState(null)
-
-  const fetchDocInfo = async () => {
-    const docInfo = doctors.find(doc => doc._id === docId)
-    setDocInfo(docInfo)
-    console.log(docInfo)
+  // Fetch doctor info based on the updated docId
+  const fetchDocInfo = () => {
+    const doctor = doctors.find(doc => doc._id === docId)
+    setDocInfo(doctor)
   }
 
-  useEffect(()=>{
-    fetchDocInfo()
-  },[doctors.docId])
+  // Fetch available slots for the doctor
+  const getAvailableSlots = () => {
+    setDocSlots([])
+    let today = new Date()
 
+    for (let i = 0; i < 7; i++) {
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate() + i)
+
+      const dayOfWeek = currentDate.getDay()
+      if (dayOfWeek < 0 || dayOfWeek > 4) continue
+
+      let endTime = new Date()
+      endTime.setDate(today.getDate() + i)
+      endTime.setHours(21, 0, 0, 0)
+
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+
+      setDocSlots(prev => ([...prev, timeSlots]))
+    }
+  }
+
+  // Actualiza la información del médico cuando el docId cambie
+  useEffect(() => {
+    if (doctors.length) {
+      fetchDocInfo()
+    }
+  }, [docId, doctors]) // Escucha cambios en `docId` y en la lista de doctores
+
+  useEffect(() => {
+    if (docInfo) {
+      getAvailableSlots()
+    }
+  }, [docInfo]) // Se actualiza cuando la información del médico cambie
 
   return docInfo && (
     <div>
-        {/* ---------Detalle Doctor ------------- */}
-        <div className='flex flex-col sm:flex-row gap-4'>
-          <div>
-            <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
-          </div>
-
-          <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0 '>
-            {/* --------doc info: nombre, grado, experincia ---------- */}
-            <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
-              {docInfo.name} 
-              <img className="w-10 h-10" src={assets.verified_icon} alt="" />
-            </p>
-            <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
-              <p>{docInfo.degree} - {docInfo.speciality}</p>
-              <button className='py-0.5 px-2 border text-sm rounded-full'>{docInfo.experience} </button>
-            </div>
-            {/* --------Acerca del Doctor ----------- */}
-            <div>
-              <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
-                Acerca de <img className="w-6 h-6" src={assets.info_icon} alt="" />
-              </p>
-              <p className='text-sm text-gray-500 max-w-[700px] mt-1 '>{docInfo.about} </p>
-            </div>
-            <p>
-              Tarifa de turno: <span>{docInfo.fees} </span>
-            </p>
-          </div>
+      {/* ---------Detalle Doctor ------------- */}
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <div>
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
         </div>
+
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0 '>
+          {/* --------doc info: nombre, grado, experiencia ---------- */}
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+            {docInfo.name} 
+            <img className="w-10 h-10" src={assets.verified_icon} alt="" />
+          </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button className='py-0.5 px-2 border text-sm rounded-full'>{docInfo.experience} </button>
+          </div>
+          {/* --------Acerca del Doctor ----------- */}
+          <div>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+              Acerca de <img className="w-6 h-6" src={assets.info_icon} alt="" />
+            </p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1 '>{docInfo.about} </p>
+          </div>
+          <p className='text-gray-500 font-medium mt-4'>
+            Tarifa de turno: <span className='text-gray-600'>{currencySymbol} {docInfo.fees} </span>
+          </p>
+        </div>
+      </div>
+      {/* -------- espacios de reserva ---------- */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Agendar turno</p>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots.map((item, index) => (
+              <div onClick={() => setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {docSlots.length && docSlots[slotIndex].map((item, index) => (
+            <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+              {item.time.toLowerCase()}
+            </p>
+          ))}
+        </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Reservar un turno</button>
+      </div>
+
+      {/* Listado de médicos relacionados */}
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
     </div>
   )
 }
 
 export default Appointment
+
+
+
+
+
+// import React, { useContext, useEffect, useState } from 'react'
+// import { useParams } from 'react-router-dom'
+// import { AppContext } from '../context/AppContext'
+// import { assets } from '../assets/assets'
+// import RelatedDoctors from '../components/Relateddoctors'
+
+// const Appointment = () => {
+
+//   const {docId} = useParams()
+//   const {doctors,currencySymbol} = useContext(AppContext)
+//   const daysOfWeek = ['LUN', 'MAR', 'MIE', 'JUE', 'VIE']
+
+
+//   const [docInfo,setDocInfo] = useState(null)
+//   const [docSlots,setDocSlots] = useState([])
+//   const [slotIndex,setSlotIndex] = useState(0)
+//   const [slotTime,setSlotTime] = useState('')
+
+//   const fetchDocInfo = async () => {
+//     const docInfo = doctors.find(doc => doc._id === docId)
+//     setDocInfo(docInfo)
+   
+//   }
+
+//   const getAvailableSlots = async () => {
+//     setDocSlots([])
+
+//     // obteniendo la fecha actual
+//     let today = new Date()
+
+//     for (let i = 0; i < 7; i++){
+//       //obteniendo fecha con índice
+//       let currentDate = new Date(today)
+//       currentDate.setDate(today.getDate() + i)
+
+//       // Verifica si el día es de lunes (0) a viernes (4)
+//     const dayOfWeek = currentDate.getDay()
+//     if (dayOfWeek < 0 || dayOfWeek > 4) continue // saltar si es sábado o domingo
+      
+//     //establecer la hora de finalización de la fecha con índice
+//     let endTime = new Date()
+//     endTime.setDate(today.getDate() + i)
+//     endTime.setHours(21, 0, 0, 0)
+
+//     // horas de ajuste
+//     if (today.getDate() === currentDate.getDate()) {
+//       currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+//       currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+//       } else {
+//         currentDate.setHours(10)
+//         currentDate.setMinutes(0)
+//       }
+
+//       let timeSlots = []
+
+//       while(currentDate < endTime) {
+//         let formattendTime = currentDate.toLocaleTimeString([], {hour:'2-digit', minute:'2-digit' })
+      
+//         // agregar ranura a la matriz
+//         timeSlots.push({
+//           datetime: new Date(currentDate),
+//           time:formattendTime
+//         })
+
+//         // Incrementar el tiempo actual en 30 minutos
+//         currentDate.setMinutes(currentDate.getMinutes() + 30)
+//       }
+
+//       setDocSlots(prev => ([...prev, timeSlots]) )
+
+//     }
+
+//   }
+
+
+
+//   useEffect(()=>{
+//     fetchDocInfo()
+//   },[doctors.docId])
+
+//   useEffect(()=>{
+//     getAvailableSlots()
+//   },[docInfo])
+
+//   useEffect(()=>{
+
+//   }, [docSlots])
+
+
+//   return docInfo && (
+//     <div>
+//         {/* ---------Detalle Doctor ------------- */}
+//         <div className='flex flex-col sm:flex-row gap-4'>
+//           <div>
+//             <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+//           </div>
+
+//           <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0 '>
+//             {/* --------doc info: nombre, grado, experincia ---------- */}
+//             <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+//               {docInfo.name} 
+//               <img className="w-10 h-10" src={assets.verified_icon} alt="" />
+//             </p>
+//             <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+//               <p>{docInfo.degree} - {docInfo.speciality}</p>
+//               <button className='py-0.5 px-2 border text-sm rounded-full'>{docInfo.experience} </button>
+//             </div>
+//             {/* --------Acerca del Doctor ----------- */}
+//             <div>
+//               <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+//                 Acerca de <img className="w-6 h-6" src={assets.info_icon} alt="" />
+//               </p>
+//               <p className='text-sm text-gray-500 max-w-[700px] mt-1 '>{docInfo.about} </p>
+//             </div>
+//             <p className='text-gray-500 font-medium mt-4'>
+//               Tarifa de turno: <span className='text-gray-600'>{currencySymbol} {docInfo.fees} </span>
+//             </p>
+//           </div>
+//         </div>
+//         {/* -------- espacios de reserva ---------- */}
+//         <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+//           <p>Agendar turno</p>
+//           <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+//             {
+//               docSlots.length && docSlots.map((item,index)=>(
+//                 <div onClick={()=> setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'} `} key={index}>
+//                   <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+//                   <p>{item[0] && item[0].datetime.getDate()} </p>
+//                 </div>
+//               ))
+//             }
+//           </div>
+//           <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+//             {docSlots.length && docSlots[slotIndex].map((item,index)=>(
+//               <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300' }`} key={index}>
+//                 {item.time.toLowerCase()}
+//               </p>
+//             ))}
+//           </div>
+//           <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Reservar un turno</button>
+//         </div>
+
+//         {/* Listado de médicos relacionados */}
+//         <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
+
+//     </div>
+//   )
+// }
+
+// export default Appointment
