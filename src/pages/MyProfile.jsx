23c8c5fb225d821d87e512586b577e6901c8db88
Matias Larumbe <matias_larumbe@hotmail.com>
2024-10-08 00:21:52 -0300

    actualizaciones en My-profile y diseño

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -17,47 +17,37 @@ const MyProfile = () => {
   const [isEdit, setIsEdit] = useState(false);
 
   return (
-    <div>
-      <img src={userData.image} alt="" />
+    <div className="max-w-lg flex flex-col gap-2 text-sm">
+      <img className="w-36 rounded" src={userData.image} alt="" />
 
       {isEdit ? (
-        <input
-          type="text"
-          value={userData.name}
-          onChange={(e) =>
+        <input className="bg-gray-50 text-3xl font-medium max-w-60 mt-4" type="text" value={userData.name} onChange={(e) =>
             setUserData((prev) => ({ ...prev, name: e.target.value }))
           }
         />
       ) : (
-        <p>{userData.name} </p>
+        <p className="font-medium text-3xl text-neutral-800 mt-4">{userData.name} </p>
       )}
-      <hr />
+      <hr className="bg-zinc-400 h-[1px] border-none " />
       <div>
-        <p>INFORMACION DE CONTACTO</p>
+        <p className="text-neutral-500 underline mt-3">INFORMACION DE CONTACTO</p>
 
-        <div>
-          <p>Email:</p>
-          <p>{userData.email} </p>
-          <p>Telefono:</p>
+        <div className="grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
+          <p className="font-medium">Email:</p>
+          <p className="text-blue-500">{userData.email} </p>
+          <p className="font-medium">Telefono:</p>
           {isEdit ? (
-            <input
-              type="text"
-              value={userData.phone}
-              onChange={(e) =>
+            <input className="bg-gray-100 max-w-52" type="text" value={userData.phone} onChange={(e) =>
                 setUserData((prev) => ({ ...prev, phone: e.target.value }))
               }
             />
           ) : (
-            <p>{userData.phone} </p>
+            <p className="text-blue-400">{userData.phone} </p>
           )}
-          <p>Direccion:</p>
+          <p className="font-medium">Direccion:</p>
           {isEdit ? (
             <p>
-              <input
-                onChange={(e) =>
-                  setUserData((prev) => ({
-                    ...prev,
-                    address: { ...prev.address, linea1: e.target.value },
+              <input className="bg-gray-50" onChange={(e) => setUserData((prev) => ({...prev, address: { ...prev.address, linea1: e.target.value },
                   }))
                 }
                 value={userData.address.linea1}
@@ -66,13 +56,13 @@ const MyProfile = () => {
               <br />
             </p>
           ) : (
-            <p>{userData.address.linea1}</p>
+            <p className="text-gray-500">{userData.address.linea1}</p>
           )}
         </div>
       </div>
       <div>
-        <p>INFORMACION BASICA</p>
-        <div>
+        <p className="text-neutral-500 underline mt-3">INFORMACION BASICA</p>
+        <div className="grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
           <p>Genero:</p>
           {isEdit ? <select onChange={(e)=> setUserData(prev =>({...prev,gender: e.target.value}))} value={userData.gender}>
             <option value="Masculino">Masculino</option>
